ca3b04d61b3b91ae8842b4e2e74b28c3ffd61cab
Selyuu <rainierlouis@gmail.com>
2018-01-02 22:21:38 +0100

    feat: Balance changed to include icon BTC

--- client/app/components/Balance/Balance.js
+++ client/app/components/Balance/Balance.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
 import { Divider, Text } from 'react-native-elements';
+
+import { images } from '../CardItem/icons/CoinIcons';
 
 import styles from './styles';
 
-const TEMP_AMOUNT = '0.001360';
-
-const Balance = () => (
+const Balance = ({ balanceAmount }) => (
 	<View style={styles.container}>
 		<Text style={styles.balanceTextTitle}>Your Balance</Text>
-		<Text h2 style={styles.balanceTextAmount}>{TEMP_AMOUNT} ฿T</Text>
+		<Text h2 style={styles.balanceTextAmount}>{balanceAmount}
+			<Image
+				style={styles.image}
+				source={{ uri: images.BTC }}
+				resizeMode='contain'
+			/>
+		</Text>
 		<Divider style={styles.divider} />
 	</View>
 )
 
 Balance.propTypes = {
-
+	balanceAmount: PropTypes.number,
 }
 
 export default Balance;
--- client/app/components/Balance/styles.js
+++ client/app/components/Balance/styles.js
@@ -2,7 +2,8 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 
 export default EStyleSheet.create({
 	container: {
-		justifyContent: 'center',
+		justifyContent: 'space-between',
+
 	},
 	balanceTextTitle: {
 		color: '$darkGrey',
@@ -10,9 +11,20 @@ export default EStyleSheet.create({
 	balanceTextAmount: {
 		color: '$white',
 	},
+	conversionText: {
+		color: '$white',
+		fontSize: 10,
+		alignSelf: 'center',
+		paddingTop: 10,
+	},
 	divider: {
 		backgroundColor: '$border',
 		width: 200,
 		alignSelf: 'center',
+	},
+	image: {
+		height: 30,
+		width: 30,
+		paddingLeft: 20,
 	}
 })
