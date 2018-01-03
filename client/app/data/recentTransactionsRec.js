b994e79e185e51d0a3b3fe54eebe3206c6b49fe0
Selyuu <rainierlouis@gmail.com>
2018-01-03 17:06:20 +0100

    misc: mock data implemented for transactions api call

---
+++ client/app/data/recentTransactionsRec.js
@@ -0,0 +1,144 @@
+export default {
+    "status": "success",
+    "data": {
+        "network": "BTCTEST",
+        "txs": [
+            {
+                "txid": "a3b4eb790c2ee4b8a0bcd3f802c422d4a771dfe1c9f6cbfdb92ff27b9d048ef9",
+                "from_green_address": false,
+                "time": 1514987100,
+                "confirmations": 0,
+                "amounts_received": [
+                    {
+                        "recipient": "2N5wGeBMZZeAVozrK8aPRFNCzFBMxjsfc5p",
+                        "amount": "0.00400000"
+                    }
+                ],
+                "senders": [
+                    "mrtYWHTsaaupJNbeABwFwmpwMemAvuEcQm"
+                ],
+                "confidence": 0.67,
+                "propagated_by_nodes": 45
+            },
+            {
+                "txid": "0ab442a6a6cfa5b71a007589de70a5854a4769e27312e8fedd72ed265d38236b",
+                "from_green_address": false,
+                "time": 1514987096,
+                "confirmations": 0,
+                "amounts_received": [
+                    {
+                        "recipient": "2N5wGeBMZZeAVozrK8aPRFNCzFBMxjsfc5p",
+                        "amount": "0.00400000"
+                    }
+                ],
+                "senders": [
+                    "mw9UsdXLi6j1gdrKLafsLEpTkzYnqCu8EW"
+                ],
+                "confidence": 0.67,
+                "propagated_by_nodes": 45
+            },
+            {
+                "txid": "c98761bee422d786ce70e1ea43e07d14ed90e225e1aad521bdea02db8e5fc86b",
+                "from_green_address": false,
+                "time": 1514987090,
+                "confirmations": 1,
+                "amounts_received": [
+                    {
+                        "recipient": "2N5wGeBMZZeAVozrK8aPRFNCzFBMxjsfc5p",
+                        "amount": "0.00400000"
+                    }
+                ],
+                "senders": [
+                    "mzZkk9Yd4BQDGuAzGaMqtgCn1XZtqrStLv"
+                ],
+                "confidence": 1,
+                "propagated_by_nodes": null
+            },
+            {
+                "txid": "27168932ecd474ece4c1e650a0510a3adc9f8c7a7c395c31c75c772e31225175",
+                "from_green_address": false,
+                "time": 1514987082,
+                "confirmations": 1,
+                "amounts_received": [
+                    {
+                        "recipient": "2N5wGeBMZZeAVozrK8aPRFNCzFBMxjsfc5p",
+                        "amount": "0.00400000"
+                    }
+                ],
+                "senders": [
+                    "mi6o9ufdTpMLAKAC4BdJf66wQuVpsuCDjH"
+                ],
+                "confidence": 1,
+                "propagated_by_nodes": null
+            },
+            {
+                "txid": "8ecadedd22b53bd2fbbc1fbb4207fb5707b0d95746163d6cab4e0acdd4db8292",
+                "from_green_address": false,
+                "time": 1514987070,
+                "confirmations": 1,
+                "amounts_received": [
+                    {
+                        "recipient": "2N5wGeBMZZeAVozrK8aPRFNCzFBMxjsfc5p",
+                        "amount": "0.00800000"
+                    }
+                ],
+                "senders": [
+                    "mt6pmWPWHKBLYeuLenS18SHfiWbiJUZRN9"
+                ],
+                "confidence": 1,
+                "propagated_by_nodes": null
+            },
+            {
+                "txid": "f756816228c212e77a416f52e4e1667b76e47335a8a7cbb82ff8509eb9e1bbc8",
+                "from_green_address": false,
+                "time": 1514987051,
+                "confirmations": 1,
+                "amounts_received": [
+                    {
+                        "recipient": "2N5wGeBMZZeAVozrK8aPRFNCzFBMxjsfc5p",
+                        "amount": "0.00800000"
+                    }
+                ],
+                "senders": [
+                    "mhiLFwxB2fc1mqxJCqdjpayuQKmP2bbYVv"
+                ],
+                "confidence": 1,
+                "propagated_by_nodes": null
+            },
+            {
+                "txid": "a174bc2fbe1bd4d83016a6ff80918541d3c14ca4ef46963e5170c18a15aa3958",
+                "from_green_address": true,
+                "time": 1514987007,
+                "confirmations": 0,
+                "amounts_received": [
+                    {
+                        "recipient": "2N5wGeBMZZeAVozrK8aPRFNCzFBMxjsfc5p",
+                        "amount": "0.00200000"
+                    }
+                ],
+                "senders": [
+                    "2NGLpBKL2w2ZrrALNrFj6Y8WwLErRTnMii5"
+                ],
+                "confidence": 0.4,
+                "propagated_by_nodes": 50
+            },
+            {
+                "txid": "17f53f615ea5129a4436f62f658085e02bbb0efef69b393ce0fbced00e62fde4",
+                "from_green_address": true,
+                "time": 1514986969,
+                "confirmations": 0,
+                "amounts_received": [
+                    {
+                        "recipient": "2N5wGeBMZZeAVozrK8aPRFNCzFBMxjsfc5p",
+                        "amount": "0.00100000"
+                    }
+                ],
+                "senders": [
+                    "2NGLpBKL2w2ZrrALNrFj6Y8WwLErRTnMii5"
+                ],
+                "confidence": 0.44,
+                "propagated_by_nodes": 45
+            }
+        ]
+    }
+}
