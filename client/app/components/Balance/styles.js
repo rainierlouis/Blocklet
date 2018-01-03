463289c93a5c708d6e4b0ce3dd592fa64f974498
Selyuu <rainierlouis@gmail.com>
2018-01-03 03:03:30 +0100

    style: balance styles added

--- client/app/components/Balance/styles.js
+++ client/app/components/Balance/styles.js
@@ -3,7 +3,7 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 export default EStyleSheet.create({
 	container: {
 		justifyContent: 'space-between',
-
+		marginTop: 50,
 	},
 	balanceTextTitle: {
 		color: '$darkGrey',
