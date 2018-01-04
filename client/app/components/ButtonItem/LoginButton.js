59f79929ba824d6319946d77c8580773037d10b7
Selyuu <rainierlouis@gmail.com>
2018-01-04 16:24:45 +0100

    fix: animatable icons removed

--- client/app/components/ButtonItem/LoginButton.js
+++ client/app/components/ButtonItem/LoginButton.js
@@ -2,26 +2,22 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Button } from 'react-native-elements';
 import PropTypes from 'prop-types';
-import * as Animatable from 'react-native-animatable';
 
 import styles from './styles';
 
 const LoginButton = ({ onPress }) => (
-	<Animatable.View
-		animation='fadeInUpBig'
-		style={styles.containerLogin}
-	>
-		<Button
-			onPress={onPress}
-			icon={{name: 'sign-in', type: 'octicon'}}
-			title='Login'
-			buttonStyle={styles.loginButton}
-		/>
-	</Animatable.View>
-)
+ <View>
+  <Button
+   onPress={onPress}
+   icon={{ name: 'sign-in', type: 'octicon' }}
+   title="Login"
+   buttonStyle={styles.loginButton}
+  />
+ </View>
+);
 
 LoginButton.propTypes = {
-	onPress: PropTypes.func,
-}
+ onPress: PropTypes.func
+};
 
 export default LoginButton;
--- client/app/components/ButtonItem/SignupButton.js
+++ client/app/components/ButtonItem/SignupButton.js
@@ -2,26 +2,22 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Button } from 'react-native-elements';
 import PropTypes from 'prop-types';
-import * as Animatable from 'react-native-animatable';
 
 import styles from './styles';
 
 const SignupButton = ({ onPress }) => (
-	<Animatable.View
-		animation='fadeInUpBig'
-		delay={500}
-	>
-		<Button
-			onPress={onPress}
-			icon={{name: 'shield', type: 'octicon'}}
-			title='Sign Up'
-			buttonStyle={styles.signupButton}
-		/>
-	</Animatable.View>
-)
+ <View>
+  <Button
+   onPress={onPress}
+   icon={{ name: 'shield', type: 'octicon' }}
+   title="Sign Up"
+   buttonStyle={styles.signupButton}
+  />
+ </View>
+);
 
 SignupButton.propTypes = {
-	onPress: PropTypes.func,
-}
+ onPress: PropTypes.func
+};
 
 export default SignupButton;
