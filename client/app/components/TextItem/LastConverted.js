6e5b30347c405f7e798e8bbfeeff2038a6e98f57
Selyuu <rainierlouis@gmail.com>
2018-01-04 16:26:18 +0100

    fix: added logic to styling and fixed decimal numbering

--- client/app/components/TextItem/LastConverted.js
+++ client/app/components/TextItem/LastConverted.js
@@ -5,17 +5,23 @@ import PropTypes from 'prop-types';
 
 import styles from './styles';
 
-const LastConverted = ({ baseCurrency, quoteCurrency, conversionRate, currentDate }) => (
-	<Text style={styles.text}>
-		1 {baseCurrency} - {conversionRate} {quoteCurrency} as of {moment(currentDate).format('MMMM D, YYYY')}
-	</Text>
+const LastConverted = ({
+ baseCurrency,
+ quoteCurrency,
+ conversionRate,
+ currentDate
+}) => (
+ <Text style={styles.text}>
+  1 {baseCurrency} - {conversionRate.toFixed(2)} {quoteCurrency} as of{' '}
+  {moment(currentDate).format('MMMM D, YYYY')}
+ </Text>
 );
 
 LastConverted.propTypes = {
-	currentDate: PropTypes.object,
-	baseCurrency: PropTypes.string,
-	quoteCurrency: PropTypes.string,
-	conversionRate: PropTypes.number,
-}
+ currentDate: PropTypes.object,
+ baseCurrency: PropTypes.string,
+ quoteCurrency: PropTypes.string,
+ conversionRate: PropTypes.number
+};
 
 export default LastConverted;
