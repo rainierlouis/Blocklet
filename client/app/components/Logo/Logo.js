f17706e259fb893197ac2ffb501a1e706d369bff
Selyuu <rainierlouis@gmail.com>
2018-01-04 21:54:03 +0100

    style: logo margin + size increased

--- client/app/components/Logo/Logo.js
+++ client/app/components/Logo/Logo.js
@@ -1,43 +1,48 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { View, Text, Keyboard, Platform, StyleSheet, Image } from 'react-native';
+import {
+ View,
+ Text,
+ Keyboard,
+ Platform,
+ StyleSheet,
+ Image
+} from 'react-native';
 import * as Animatable from 'react-native-animatable';
 
 import styles from './styles';
 
 class Logo extends Component {
-  static propTypes = {
+ static propTypes = {};
 
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-							<View style={styles.containerImage}>
-								<Animatable.Image
-									animation='bounceInLeft'
-									easing='ease'
-									duration={2000}
-									delay={500}
-									iterationCount={1}
-									resizeMode='contain'
-									style={[StyleSheet.absoluteFill, styles.containerImage]}
-									source={require('./img/background.png')}
-								/>
-								<Animatable.Image
-									animation='bounceInRight'
-									easing='ease'
-									duration={2000}
-									delay={500}
-									iterationCount={1}
-									resizeMode='contain'
-									style={styles.logo}
-									source={require('./img/coin.png')}
-								/>
-							</View>
-      </View>
-    );
-  }
+ render() {
+  return (
+   <View style={styles.container}>
+    <View style={styles.containerImage}>
+     {/* <Animatable.Image
+      animation="bounceInLeft"
+      easing="ease"
+      duration={2000}
+      delay={500}
+      iterationCount={1}
+      resizeMode="contain"
+      style={[StyleSheet.absoluteFill, styles.containerImage]}
+      source={require('./img/background.png')}
+     /> */}
+     <Animatable.Image
+      animation="bounceInDown"
+      easing="ease"
+      duration={2000}
+      delay={500}
+      iterationCount={1}
+      resizeMode="contain"
+      style={styles.logo}
+      source={require('./img/coin.png')}
+     />
+    </View>
+   </View>
+  );
+ }
 }
 
 export default Logo;
--- client/app/components/Logo/styles.js
+++ client/app/components/Logo/styles.js
@@ -4,17 +4,17 @@ import { Dimensions } from 'react-native';
 const imageWidth = Dimensions.get('window').width / 2.5;
 
 export default EStyleSheet.create({
-  container: {
-    alignItems: 'center',
-				marginBottom: 30,
-  },
-  containerImage: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: imageWidth,
-    height: imageWidth,
-  },
-  logo: {
-    width: imageWidth / 2,
-  },
-})
+ container: {
+  alignItems: 'center',
+  marginTop: -50
+ },
+ containerImage: {
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: imageWidth,
+  height: imageWidth
+ },
+ logo: {
+  width: imageWidth
+ }
+});
