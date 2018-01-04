10028f85403df836710ba8c472cf94efabd78400
Selyuu <rainierlouis@gmail.com>
2018-01-04 21:53:39 +0100

    style: input fields placeholder font styled

--- client/app/components/FormField/LoginField.js
+++ client/app/components/FormField/LoginField.js
@@ -5,18 +5,42 @@ import { FormLabel, FormInput } from 'react-native-elements';
 
 import styles from './styles';
 
-const LoginField = () => (
-
-		<KeyboardAvoidingView
-			style={styles.container}
-			behavior='padding'
-		>
-			<FormLabel>Username</FormLabel>
-			<FormInput />
-			<FormLabel>Password</FormLabel>
-			<FormInput secureTextEntry={true}/>
-		</KeyboardAvoidingView>
-
-)
+const LoginField = ({ onPress }) => (
+ <KeyboardAvoidingView style={styles.container} behavior="padding">
+  {/* <FormLabel containerStyle={styles.formLabel} labelStyle={{ color: 'white' }}>
+   Username
+  </FormLabel> */}
+  <TextInput
+   placeholder="Email"
+   style={styles.textInput}
+   keyboardType="email-address"
+   keyboardAppearance="dark"
+   returnKeyType="next"
+   onSubmitEditing={event => {
+    this.passwordRef.focus();
+   }}
+  />
+  {/* <FormInput
+   keyboardType="email-address"
+   keyboardAppearance="dark"
+   returnKeyType="next"
+   onSubmitEditing={event => {
+    this.passwordRef.focus();
+   }}
+  /> */}
+  {/* <FormLabel containerStyle={styles.formLabel} labelStyle={{ color: 'white' }}>
+   Password
+  </FormLabel> */}
+  <TextInput
+   placeholder="Password"
+   style={styles.textInput}
+   ref={passwordRef => (this.passwordRef = passwordRef)}
+   secureTextEntry={true}
+   onSubmitEditing={onPress}
+   keyboardAppearance="dark"
+   returnKeyType="go"
+  />
+ </KeyboardAvoidingView>
+);
 
 export default LoginField;
--- client/app/components/FormField/styles.js
+++ client/app/components/FormField/styles.js
@@ -1,8 +1,24 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 export default EStyleSheet.create({
-	container: {
-		width: 350,
-		paddingTop: 100,
-	}
-})
+ container: {
+  width: 350,
+  paddingTop: 100,
+  marginTop: 200
+ },
+ formLabel: {
+  backgroundColor: 'transparent',
+  flexDirection: 'row'
+ },
+ textInput: {
+  marginVertical: 0.5,
+  alignSelf: 'center',
+  backgroundColor: 'white',
+  height: 50,
+  width: 250,
+  borderBottomColor: '$darkGrey',
+  borderBottomWidth: 1,
+  paddingLeft: 15,
+  fontFamily: 'quicksand'
+ }
+});
