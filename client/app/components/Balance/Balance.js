a8f64d64aa3d00f9afb77eebed3d98f980141f8d
Selyuu <rainierlouis@gmail.com>
2018-01-04 16:24:18 +0100

    feat: animatable icon

--- client/app/components/Balance/Balance.js
+++ client/app/components/Balance/Balance.js
@@ -2,27 +2,31 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { View, Image } from 'react-native';
 import { Divider, Text } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 import { images } from '../CardItem/icons/CoinIcons';
 
 import styles from './styles';
 
 const Balance = ({ balanceAmount }) => (
-	<View style={styles.container}>
-		<Text style={styles.balanceTextTitle}>Your Balance</Text>
-		<Text h2 style={styles.balanceTextAmount}>{balanceAmount}
-			<Image
-				style={styles.image}
-				source={{ uri: images.BTC }}
-				resizeMode='contain'
-			/>
-		</Text>
-		<Divider style={styles.divider} />
-	</View>
-)
+ <View style={styles.container}>
+  <Text style={styles.balanceTextTitle}>Your Balance</Text>
+  <Text h3 style={styles.balanceTextAmount}>
+   {balanceAmount}
+   <Animatable.Image
+    animation="rotate"
+    iterationCount={2}
+    style={styles.image}
+    source={{ uri: images.BTC }}
+    resizeMode="contain"
+   />
+  </Text>
+  <Divider style={styles.divider} />
+ </View>
+);
 
 Balance.propTypes = {
-	balanceAmount: PropTypes.number,
-}
+ balanceAmount: PropTypes.number
+};
 
 export default Balance;
