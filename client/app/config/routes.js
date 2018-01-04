2bcb4648492f12412f7de2c9bc939570f7ab7b60
Selyuu <rainierlouis@gmail.com>
2018-01-04 21:51:13 +0100

    fix: route structure adjusted

--- client/app/config/routes.js
+++ client/app/config/routes.js
@@ -6,53 +6,56 @@ import Login from '../screens/Login';
 import Loading from '../screens/Loading';
 import Transactions from '../screens/Transactions';
 
-// const LoadingStack = StackNavigator({
-// 	Loading: {
-// 		screen: Loading,
-// 	},
-// 	Hub: {
-// 		screen: HomeStack,
-// 	}
-// })
-
 const HomeStack = StackNavigator({
-	Hub: {
-		screen: Hub,
-		navigationOptions: {
-			header: () => null,
-		}
-	},
-	MenuList: {
-		screen: MenuList,
-		navigationOptions: {
-			headerTitle: 'Menu',
-			headerStyle: {backgroundColor: '#43314f'},
-			headerTitleStyle: {color: 'white'},
-			headerTintColor: 'white',
-		}
-	},
-	Transactions: {
-		screen: Transactions,
-		navigationOptions: {
-			headerTitle: 'Transactions',
-			headerStyle: {backgroundColor: '#43314f'},
-			headerTitleStyle: {color: 'white'},
-			headerTintColor: 'white',
-		}
-	}
-})
+ Hub: {
+  screen: Hub,
+  navigationOptions: {
+   header: () => null
+  }
+ },
+ MenuList: {
+  screen: MenuList,
+  navigationOptions: {
+   headerTitle: 'Menu',
+   headerStyle: { backgroundColor: '#43314f' },
+   headerTitleStyle: { color: 'white' },
+   headerTintColor: 'white'
+  }
+ },
+ Transactions: {
+  screen: Transactions,
+  navigationOptions: {
+   headerTitle: 'Transactions',
+   headerStyle: { backgroundColor: '#43314f' },
+   headerTitleStyle: { color: 'white' },
+   headerTintColor: 'white'
+  }
+ }
+});
 
-export default StackNavigator({
-	Login: {
-		screen: Login,
-	},
-	Hub: {
-		screen: HomeStack,
-		navigationOptions: {
-			gesturesEnabled: false,
-		}
-	},
-}, {
-	mode: 'modal',
-	headerMode: 'none',
-})
+// const LoadingStack = StackNavigator({
+//  Loading: {
+//   screen: Loading
+//  },
+//  Hub: {
+//   screen: HomeStack
+//  }
+// });
+
+export default StackNavigator(
+ {
+  Login: {
+   screen: Login
+  },
+  Loading: {
+   screen: HomeStack,
+   navigationOptions: {
+    gesturesEnabled: false
+   }
+  }
+ },
+ {
+  mode: 'modal',
+  headerMode: 'none'
+ }
+);
