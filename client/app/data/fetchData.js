50786c09e6c0750f9c84468c7e5199d4724d7533
Selyuu <rainierlouis@gmail.com>
2018-01-04 16:22:54 +0100

    feat: fetch method implemented for balance + ticker + conversion rate

---
+++ client/app/data/fetchData.js
@@ -0,0 +1,29 @@
+const API_KEY_TESTNET = 'f737-19e7-0636-ebbc';
+const API_KEY = '98d2-7443-af3f-17e4';
+const PIN = '12112015K';
+
+// FETCH API CALLS //
+// GET new address with label (label)
+// GET balance of specified address (address)
+// POST transfer of coin (amount, fromAddress, toAddress, pin)
+// GET current btc rate in popular currencies
+// GET 24h + 7d percent change (https://api.coinmarketcap.com/v1/ticker/)
+// GET 25 recent sent transactions (sent type, address)
+// GET 25 recent received transactions (received type, address)
+
+export const fetchBalance = address =>
+ fetch(
+  `https://block.io/api/v2/get_address_balance/?api_key=${API_KEY_TESTNET}&addresses=${address}`
+ )
+  .then(response => response.json())
+  .then(data => data.data.available_balance);
+
+export const fetchBtcRate = () =>
+ fetch(`https://block.io/api/v2/get_current_price/?api_key=${API_KEY}`)
+  .then(response => response.json())
+  .then(data => data.data);
+
+export const fetchTickerRate = currency =>
+ fetch(`https://api.coinmarketcap.com/v1/ticker/`)
+  .then(response => response.json())
+  .then(data => data[0]);
