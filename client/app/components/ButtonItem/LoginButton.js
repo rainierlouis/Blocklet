7e8c0975cadcf719a5b380c62bb517e028e8595e
Selyuu <rainierlouis@gmail.com>
2018-01-04 21:53:02 +0100

    style: login button styled further

--- client/app/components/ButtonItem/LoginButton.js
+++ client/app/components/ButtonItem/LoginButton.js
@@ -9,9 +9,12 @@ const LoginButton = ({ onPress }) => (
  <View>
   <Button
    onPress={onPress}
-   icon={{ name: 'sign-in', type: 'octicon' }}
-   title="Login"
+   // icon={{ name: 'sign-in', type: 'octicon' }}
+   title="Enter"
    buttonStyle={styles.loginButton}
+   fontFamily="quicksand"
+   color="white"
+   fontSize={22}
   />
  </View>
 );
