997086ddf6c66a8d643ac990ba8ab081e4d2dc9b
Selyuu <rainierlouis@gmail.com>
2018-01-09 15:51:02 +0100

    refactor: removed bloated code

---
+++ client/app/data/userData.js
@@ -0,0 +1,23 @@
+// btc
+export const BTC = {
+ currency: 'BTC',
+ API_KEY_TESTNET: 'f737-19e7-0636-ebbc',
+ API_KEY: '98d2-7443-af3f-17e4',
+ ADDRESS: '2N5wGeBMZZeAVozrK8aPRFNCzFBMxjsfc5p'
+};
+
+// ltc
+export const LTC = {
+ currency: 'LTC',
+ API_KEY_TESTNET: '2857-f4a9-d944-c5fa',
+ API_KEY: '1e47-79a6-9797-e570',
+ ADDRESS: '2MxtpzTF5b5UwdK7qJstD1NnZMMedi5zy2L'
+};
+
+// doge
+export const DOGE = {
+ currency: 'DOGE',
+ API_KEY_TESTNET: '119e-0781-247d-7297',
+ API_KEY: 'b108-0d58-ccff-c468',
+ ADDRESS: '2N8jnDQH9KqidrqQ4veTfAiSgh5dNNSgWr1'
+};
