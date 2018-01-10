374ddbd993ae2b7f18dede6624a28fa0c10142b2
Selyuu <rainierlouis@gmail.com>
2018-01-10 12:09:37 +0100

    feat: implemented currency name

--- client/app/data/userData.js
+++ client/app/data/userData.js
@@ -1,6 +1,7 @@
 // btc
 export const BTC = {
  currency: 'BTC',
+ currency_name: 'Bitcoin',
  API_KEY_TESTNET: 'f737-19e7-0636-ebbc',
  API_KEY: '98d2-7443-af3f-17e4',
  ADDRESS: '2N5wGeBMZZeAVozrK8aPRFNCzFBMxjsfc5p'
@@ -9,6 +10,7 @@ export const BTC = {
 // ltc
 export const LTC = {
  currency: 'LTC',
+ currency_name: 'Bitcoin',
  API_KEY_TESTNET: '2857-f4a9-d944-c5fa',
  API_KEY: '1e47-79a6-9797-e570',
  ADDRESS: '2MxtpzTF5b5UwdK7qJstD1NnZMMedi5zy2L'
@@ -17,6 +19,7 @@ export const LTC = {
 // doge
 export const DOGE = {
  currency: 'DOGE',
+ currency_name: 'Bitcoin',
  API_KEY_TESTNET: '119e-0781-247d-7297',
  API_KEY: 'b108-0d58-ccff-c468',
  ADDRESS: '2N8jnDQH9KqidrqQ4veTfAiSgh5dNNSgWr1'
