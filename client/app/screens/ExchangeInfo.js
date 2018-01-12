f9504209ecd3de799ef8d4e8fe760d0febb99a07
Selyuu <rainierlouis@gmail.com>
2018-01-12 16:54:50 +0100

    style: further styled

--- client/app/screens/ExchangeInfo.js
+++ client/app/screens/ExchangeInfo.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import { FlatList, View, StatusBar } from 'react-native';
 import PropTypes from 'prop-types';
 import { NavigationActions } from 'react-navigation';
+import { Text } from 'react-native-elements';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
 import { Container } from '../components/Container';
 import { HeaderTop } from '../components/HeaderTop';
-import { ExchangeItem } from '../components/ListItem';
+import { ExchangeBox } from '../components/ExchangeBox';
 
-import { fetchExchange } from '../data/fetchData';
+import { fetchTickerRate } from '../data/fetchData';
+import { fetch1h, fetch24h, fetch7d, fetch1m } from '../data/graphData';
+
+import WS from 'react-native-websocket';
 
 // REDUX
 import { connect } from 'react-redux';
@@ -18,15 +23,34 @@ const resetAction = NavigationActions.reset({
  actions: [NavigationActions.navigate({ routeName: 'Home' })]
 });
 
-class Transactions extends Component {
+const styles = EStyleSheet.create({
+ exchContainer: {
+  flexDirection: 'row'
+ }
+});
+
+class ExchangeInfo extends Component {
  constructor(props) {
   super(props);
-  fetchExchange().then(data => this.props.addExchange(data));
  }
 
  static propTypes = {
+  onPressMenu: PropTypes.func,
   onPress: PropTypes.func
  };
+
+ componentDidMount() {
+  // Tickers
+  // fetchTickerRate('Bitcoin').then(data => this.props.addTickers(data));
+  // 1h
+  // fetch1h().then(data => this.props.add1h(data['BTC']));
+  // 24h
+  // fetch24h().then(data => this.props.add24h(data['BTC']));
+  // // 7d
+  // fetch7d().then(data => this.props.add7d(data['BTC']));
+  // // 1m
+  // fetch1m().then(data => this.props.add1m(data['BTC']));
+ }
 
  pressMenu = () => {
   this.props.navigation.navigate('MenuList', {
@@ -41,18 +65,34 @@ class Transactions extends Component {
  // RENDER ========================
 
  render() {
-  console.log('EXCHANGE=======', this.props.exchange);
+  // console.log('24h ticker================', this.props.ticker24h);
   return (
    <Container>
     <StatusBar translucent={false} barStyle="light-content" />
     <HeaderTop onPressMenu={this.pressMenu} onPressHome={this.pressHome} />
-    <FlatList
-     style={{ marginTop: 80 }}
-     data={this.props.exchange}
-     keyExtractor={(item, i) => i}
-     renderItem={({ item }) => (
-      <ExchangeItem price={item.price} priceBase={item.price_base} />
-     )}
+    <View style={styles.exchContainer} />
+    <ExchangeBox
+     coin={null}
+     // 1h
+     time1h={'1h'}
+     timeText1h={'1 Hour'}
+     ticker1h={this.props.ticker1h}
+     data1h={this.props.data1h}
+     // 24h
+     time24h={'24h'}
+     timeText24h={'24 Hours'}
+     ticker24h={this.props.ticker24h}
+     data24h={this.props.data24h}
+     // 7d
+     time7d={'7d'}
+     timeText7d={'7 Days'}
+     ticker7d={this.props.ticker7d}
+     data7d={this.props.data7d}
+     // 1m
+     time1m={'1m'}
+     timeText1m={'1 Month'}
+     ticker1m={this.props.ticker1m}
+     data1m={this.props.data1m}
     />
    </Container>
   );
@@ -61,16 +101,43 @@ class Transactions extends Component {
 
 const mapStateToProps = state => {
  return {
-  exchange: state.exchangeReducers.exchange
+  data1h: state.exchangeReducers.data1h,
+  ticker1h: state.exchangeReducers.ticker1h,
+  data24h: state.exchangeReducers.data24h,
+  ticker24h: state.exchangeReducers.ticker24h,
+  data7d: state.exchangeReducers.data7d,
+  ticker7d: state.exchangeReducers.ticker7d,
+  data1m: state.exchangeReducers.data1m,
+  ticker1m: state.exchangeReducers.ticker1m
  };
 };
 
 const mapDispatchToProps = dispatch => ({
- addExchange: data =>
+ addTickers: data =>
   dispatch({
-   type: 'ADD_EXCH',
+   type: 'TICKER',
+   data
+  }),
+ add1h: data =>
+  dispatch({
+   type: 'DATA_1H',
+   data
+  }),
+ add24h: data =>
+  dispatch({
+   type: 'DATA_24H',
+   data
+  }),
+ add7d: data =>
+  dispatch({
+   type: 'DATA_7D',
+   data
+  }),
+ add1m: data =>
+  dispatch({
+   type: 'DATA_1M',
    data
   })
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Transactions);
+export default connect(mapStateToProps, mapDispatchToProps)(ExchangeInfo);
